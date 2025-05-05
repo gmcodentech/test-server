e6a41ff9ef7528596cae46263cb4573c23b47716
CodingAndTechnology <CodingAndTechnology@aol.com>
2025-05-06 01:12:10 +0530

    hello handler added

--- index.go
+++ index.go
@@ -9,8 +9,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, This is A DOCKER Learner! Able to push an optimized docker image")
 }
 
+func handlerHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello,How are you?")
+}
+
 func main() {
 	fmt.Println("started server...")
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/hello", handlerHello)
 	http.ListenAndServe(":8080", nil)
 }
